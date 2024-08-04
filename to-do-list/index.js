826c15656c1f58e73377275cb2b5511d958f3704
Fizza <fizach581@gmail.com>
2024-08-05 00:49:43 +0500

    update funtionalities of edit and css

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -1,7 +1,5 @@
 const formEl = document.querySelector(".form");
-
 const inputEl = document.querySelector(".input");
-
 const ulEl = document.querySelector(".list");
 
 let list = JSON.parse(localStorage.getItem("list"));
@@ -29,15 +27,17 @@ function toDoList(task) {
   liEl.innerText = newTask;
   ulEl.appendChild(liEl);
   inputEl.value = "";
+
   const checkBtnEl = document.createElement("div");
-  checkBtnEl.innerHTML = `
-  <i class="fas fa-check-square">
-  `;
+  checkBtnEl.innerHTML = `<i class="fas fa-check-square"></i>`;
   liEl.appendChild(checkBtnEl);
+
+  const editBtnEl = document.createElement("div");
+  editBtnEl.innerHTML = `<i class="fas fa-edit"></i>`;
+  liEl.appendChild(editBtnEl);
+
   const trashBtnEl = document.createElement("div");
-  trashBtnEl.innerHTML = `
-  <i class="fas fa-trash"></i>
-  `;
+  trashBtnEl.innerHTML = `<i class="fas fa-trash"></i>`;
   liEl.appendChild(trashBtnEl);
 
   checkBtnEl.addEventListener("click", () => {
@@ -45,10 +45,43 @@ function toDoList(task) {
     updateLocalStorage();
   });
 
+  editBtnEl.addEventListener("click", () => {
+    const editInputEl = document.createElement("input");
+    editInputEl.type = "text";
+    editInputEl.value = newTask;
+    liEl.innerText = "";
+    liEl.appendChild(editInputEl);
+
+    const saveBtnEl = document.createElement("div");
+    saveBtnEl.innerHTML = `<i class="fas fa-save"></i>`;
+    liEl.appendChild(saveBtnEl);
+
+    const cancelBtnEl = document.createElement("div");
+    cancelBtnEl.innerHTML = `<i class="fas fa-times"></i>`;
+    liEl.appendChild(cancelBtnEl);
+
+    saveBtnEl.addEventListener("click", () => {
+      newTask = editInputEl.value;
+      liEl.innerText = newTask;
+      liEl.appendChild(checkBtnEl);
+      liEl.appendChild(editBtnEl);
+      liEl.appendChild(trashBtnEl);
+      updateLocalStorage();
+    });
+
+    cancelBtnEl.addEventListener("click", () => {
+      liEl.innerText = newTask;
+      liEl.appendChild(checkBtnEl);
+      liEl.appendChild(editBtnEl);
+      liEl.appendChild(trashBtnEl);
+    });
+  });
+
   trashBtnEl.addEventListener("click", () => {
     liEl.remove();
     updateLocalStorage();
   });
+
   updateLocalStorage();
 }
 
@@ -63,3 +96,4 @@ function updateLocalStorage() {
   });
   localStorage.setItem("list", JSON.stringify(list));
 }
+
