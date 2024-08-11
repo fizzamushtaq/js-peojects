6d93e017461dd29da53241ad5478a9a795be9ae9
Fizza <fizach581@gmail.com>
2024-08-12 01:44:47 +0500

    update file

--- CRUD/CRUD.js
+++ CRUD/CRUD.js
@@ -1,9 +1,6 @@
 const submit = document.querySelector("#submit");
 console.log("Submit ", submit);
 
-let preventerdata = [];
-console.log("prev enterdata", preventerdata);
-
 let enterdata = true;
 if (enterdata) {
   submit.addEventListener("click", function (e) {
@@ -14,37 +11,53 @@ if (enterdata) {
 }
 
 function validtheenterdata(enterdata) {
-  //   if (isNaN(enterdata)) {
-  //     alert("please enter a valid name ");
-  //   }
-  //   // else if (enterdata = "email") {
-  //   //     alert("please enter a email ") ;
-  //   // }
-  //   else {
-  //     preventerdata.push(enterdata);
-  //     console.log("push", preventerdata);
-  //     console.log("enterdata check ", enterdata);
-  //   }
+    if (isNaN(enterdata)) {
+      alert("please enter a valid name ");
+    }
+    // else if (enterdata = "email") {
+    //     alert("please enter a email ") ;
+    // }
+    else {
+      enterdata.push(enterdata);
+      console.log("push", enterdata);
+      console.log("enterdata check ", enterdata);
+    }
 
   // Get form values
-  const username = document.querySelector("#first-name").value;
-  console.log(username);
+  const username = document.querySelector("#first-name").value = '';
+  const lastname = document.querySelector("#last-name").value = '';
+  const email = document.querySelector("#email").value = '';
+  const dob = document.getElementById("dob").value = '';
 
-  const lastname = document.querySelector("#last-name").value;
-  console.log(lastname);
+  console.log(
+    "Before:- \n first name",
+    username,
+    "last name",
+    lastname,
+    "email:- ",
+    email,
+    "dob",
+    dob
+  );
 
-  const email = document.querySelector("#email").value;
-  console.log(email);
+  // Create an object with the form data
+  const newEntry = {
+    fname: username,
+    lname: lastname,
+    email: email,
+    dob: dob,
+  };
 
-  // const dob = document.querySelector('#dob-form').value;
-  const dob = document.getElementById("dob").value;
+  console.log("After ", newEntry);
 
-  console.log(dob);
+  // Get the existing data from localStorage
+  let storedData = JSON.parse(localStorage.getItem("userData")) || [];
 
-  // note:-
-  // Store in localStorage
-  // localStorage.setItem('userName', username);
-  // localStorage.setItem('userlastname', lastname)
-  // localStorage.setItem('userEmail', email);
-  // localStorage.setItem('userDOB', dob);
+  // Add the new entry to the array
+  storedData.push(newEntry);
+
+  // Store the updated array back in localStorage
+  localStorage.setItem("userData", JSON.stringify(storedData));
+
+  console.log("Current Stored Data: ", storedData);
 }
