c821a92a90e1e2ddb64b8209eb48ff1206d9cca6
Fizza <fizach581@gmail.com>
2024-08-11 22:10:56 +0500

    Fixed Issues

--- CRUD/CRUD.js
+++ CRUD/CRUD.js
@@ -1,43 +1,50 @@
-const submit = document.querySelector('#submit')
+const submit = document.querySelector("#submit");
 console.log("Submit ", submit);
 
-let preventerdata = []
+let preventerdata = [];
 console.log("prev enterdata", preventerdata);
 
-let enterdata = true
+let enterdata = true;
 if (enterdata) {
-    submit.addEventListener('click', function (e) {
-     e.preventDefault()
-     console.log(enterdata);
-    validtheenterdata(enterdata) ;
- }
-)}
+  submit.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log(enterdata);
+    validtheenterdata(enterdata);
+  });
+}
 
 function validtheenterdata(enterdata) {
-    if (isNaN(enterdata)) {
-        alert("please enter a valid name ");
-        } 
-        // else if (enterdata = "email") {
-        //     alert("please enter a email ") ;
-        // } 
-         else{
-            preventerdata.push(enterdata)
-            console.log("push", preventerdata)
-            console.log("enterdata check ", enterdata); 
-        }
-        }
-    
+  //   if (isNaN(enterdata)) {
+  //     alert("please enter a valid name ");
+  //   }
+  //   // else if (enterdata = "email") {
+  //   //     alert("please enter a email ") ;
+  //   // }
+  //   else {
+  //     preventerdata.push(enterdata);
+  //     console.log("push", preventerdata);
+  //     console.log("enterdata check ", enterdata);
+  //   }
 
-    // Get form values
-    const username = document.querySelector('#first-name').value;
-    const lastname  = document.querySelector('#last-name').value
-    const email = document.querySelector('#email').value;
-    const dob = document.querySelector('.dob-form').value;
+  // Get form values
+  const username = document.querySelector("#first-name").value;
+  console.log(username);
 
-    // note:- 
-    // Store in localStorage
-    localStorage.setItem('userName', username);
-    localStorage.setItem('userlastname', lastname)
-    localStorage.setItem('userEmail', email);
-    localStorage.setItem('userDOB', dob);
+  const lastname = document.querySelector("#last-name").value;
+  console.log(lastname);
 
+  const email = document.querySelector("#email").value;
+  console.log(email);
+
+  // const dob = document.querySelector('#dob-form').value;
+  const dob = document.getElementById("dob").value;
+
+  console.log(dob);
+
+  // note:-
+  // Store in localStorage
+  // localStorage.setItem('userName', username);
+  // localStorage.setItem('userlastname', lastname)
+  // localStorage.setItem('userEmail', email);
+  // localStorage.setItem('userDOB', dob);
+}
